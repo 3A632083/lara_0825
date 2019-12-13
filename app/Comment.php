4dea4435cde078e80976c6b0f7be891451329345
3A632083 <kiki87119@gmail.com>
2019-12-13 14:49:56 +0800

    練習7-3 在Comment.php增加關聯

--- app/Comment.php
+++ app/Comment.php
@@ -3,8 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Post;
+
 
 class Comment extends Model
 {
     protected $fillable = ['content'];
+
+    public function post()
+    {
+    return $this->belongsTo(Post::class);
+    }
 }
