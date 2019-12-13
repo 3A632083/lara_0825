d722c9428522a735bf1f8abd1743d87ff6da2bf3
3A632083 <kiki87119@gmail.com>
2019-12-13 14:05:07 +0800

    6-2 取得 Model $fourthPost

--- routes/web.php
+++ routes/web.php
@@ -52,5 +52,8 @@ Route::get('/', function () {
 //$allPosts=\App\Post::all();
 //dd($allPosts);
 
-$featuredPosts=\App\Post::where('is_feature',1)->get();
-dd($featuredPosts);
+//$featuredPosts=\App\Post::where('is_feature',1)->get();
+//dd($featuredPosts);
+
+$fourthPost=\App\Post::find(4);
+dd($fourthPost);
